88f1b3957710c237730ac41bcfaa398bafcf912c
Kimwel <94892701+Kheemwel@users.noreply.github.com>
2022-10-27 15:04:45 +0800

    Add files via upload

---
+++ Lab4Exer1/Beller_BasicThreading/FrmBasicThread.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Beller_BasicThreading
+{
+    partial class FrmBasicThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(12, 34);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(356, 37);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "-Before starting Thread-";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.Location = new System.Drawing.Point(140, 152);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(85, 30);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // FrmBasicThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 232);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lblText);
+            this.Name = "FrmBasicThread";
+            this.Text = "BasicThread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
