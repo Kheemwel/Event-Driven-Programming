c1eeb67b427ac84a381b132b7dba5bdfeb20f5e6
Kimwel <94892701+Kheemwel@users.noreply.github.com>
2022-10-27 15:01:24 +0800

    Add files via upload

---
+++ Lab1Exer2/CalculatorApplication/FrmCalculator.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CalculatorApplication
+{
+    partial class FrmCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxInput1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxInput2 = new System.Windows.Forms.TextBox();
+            this.cbOperator = new System.Windows.Forms.ComboBox();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.lblDisplayTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter First Number: ";
+            // 
+            // txtBoxInput1
+            // 
+            this.txtBoxInput1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxInput1.Location = new System.Drawing.Point(185, 61);
+            this.txtBoxInput1.Name = "txtBoxInput1";
+            this.txtBoxInput1.Size = new System.Drawing.Size(159, 26);
+            this.txtBoxInput1.TabIndex = 1;
+            this.txtBoxInput1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxInput1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Second Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Answer:";
+            // 
+            // txtBoxInput2
+            // 
+            this.txtBoxInput2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtBoxInput2.Location = new System.Drawing.Point(185, 152);
+            this.txtBoxInput2.Name = "txtBoxInput2";
+            this.txtBoxInput2.Size = new System.Drawing.Size(159, 26);
+            this.txtBoxInput2.TabIndex = 4;
+            this.txtBoxInput2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxInput2_KeyPress);
+            // 
+            // cbOperator
+            // 
+            this.cbOperator.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbOperator.FormattingEnabled = true;
+            this.cbOperator.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cbOperator.Location = new System.Drawing.Point(242, 107);
+            this.cbOperator.Name = "cbOperator";
+            this.cbOperator.Size = new System.Drawing.Size(53, 27);
+            this.cbOperator.TabIndex = 5;
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.AutoSize = true;
+            this.btnEqual.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEqual.Location = new System.Drawing.Point(185, 269);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(110, 32);
+            this.btnEqual.TabIndex = 6;
+            this.btnEqual.Text = "Calculate";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // lblDisplayTotal
+            // 
+            this.lblDisplayTotal.AutoSize = true;
+            this.lblDisplayTotal.Location = new System.Drawing.Point(182, 206);
+            this.lblDisplayTotal.Name = "lblDisplayTotal";
+            this.lblDisplayTotal.Size = new System.Drawing.Size(13, 13);
+            this.lblDisplayTotal.TabIndex = 7;
+            this.lblDisplayTotal.Text = "0";
+            // 
+            // FrmCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 346);
+            this.Controls.Add(this.lblDisplayTotal);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.cbOperator);
+            this.Controls.Add(this.txtBoxInput2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxInput1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmCalculator";
+            this.Text = "FrmCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxInput1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxInput2;
+        private System.Windows.Forms.ComboBox cbOperator;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.Label lblDisplayTotal;
+    }
+}
+
---
+++ Lab1Exer2/CalculatorApplication/FrmCalculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApplication
+{
+    public partial class FrmCalculator : Form
+    {
+        CalculatorClass cal;
+        double num1, num2;
+        public FrmCalculator()
+        {
+            InitializeComponent();
+            cal = new CalculatorClass();
+            num1 = 0;
+            num2 = 0;
+        }
+
+        private void txtBoxInput1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsNumber(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtBoxInput2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsNumber(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            num1 = txtBoxInput1.Text == "" ? 0 : Convert.ToDouble(txtBoxInput1.Text);
+            num2 = txtBoxInput2.Text == "" ? 0 : Convert.ToDouble(txtBoxInput2.Text);
+
+
+            switch (cbOperator.SelectedIndex)
+            {
+                case 0:
+                    cal.CalculateEvent += new Formula<double>(cal.GetSum);
+                    lblDisplayTotal.Text = cal.GetSum(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula<double>(cal.GetSum);
+                    break;
+                case 1:
+                    cal.CalculateEvent += new Formula<double>(cal.GetDifference);
+                    lblDisplayTotal.Text = cal.GetDifference(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula<double>(cal.GetDifference);
+                    break;
+                case 2:
+                    cal.CalculateEvent += new Formula<double>(cal.GetProduct);
+                    lblDisplayTotal.Text = cal.GetProduct(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula<double>(cal.GetProduct);
+                    break;
+                case 3:
+                    cal.CalculateEvent += new Formula<double>(cal.GetQuotient);
+                    lblDisplayTotal.Text = cal.GetQuotient(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula<double>(cal.GetQuotient);
+                    break;
+            }
+
+        }
+
+    }
+}
